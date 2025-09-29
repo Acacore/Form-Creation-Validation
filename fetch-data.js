f586a7d4acfa980eff2d3a379c315bf0ced23e1b
acacore <mensart81@outlook.com>
2025-09-29 17:50:01 +0000

    async function Implemented

---
+++ fetch-data.js
@@ -0,0 +1,29 @@
+
+     async function fetchUserData(params) {
+         const apiUrl = 'https://jsonplaceholder.typicode.com/users'
+         const dataContainer = document.getElementById("api-data")
+
+        try {
+            let response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const users = await response.json();
+            dataContainer.innerHTML = ''
+            const userList = document.createElement("ul")
+            users.forEach(element => {
+                let user = document.createElement("li")
+                user.innerHTML = element.name;
+                userList.append(user)
+            });
+            dataContainer.append(userList)
+            
+        } catch (error) {
+            dataContainer.innerHTML = ''
+            dataContainer.textContent = "Failed to load user data."
+        }
+}
+    
+document.addEventListener("DOMContentLoaded", function () {
+    fetchUserData()
+})
