09ce7b5e8198cdc516c847b5d05a2d02600b89ef
acacore <mensart81@outlook.com>
2025-09-29 11:33:48 +0000

    script.js altered to meet ALX requirement

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", function(){
     const registrationForm = document.getElementById("registration-form")
     const feedbackDiv = document.getElementById("form-feedback")
 
